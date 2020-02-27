b8dba40a736379f88ee17e213e89d6d0f5b772a8
lasttruth <robindoirin@gmail.com>
2020-02-27 09:43:42 -0500

    added a fetch img function

--- frontend_js/src/index.js
+++ frontend_js/src/index.js
@@ -11,16 +11,34 @@ console.log("hello world")
 
 
 
-
+//fetch the api controller with the ideas
 fetch(`${api_url}/ideas`)
   .then(response => response.json())
+  //calling a function to display on html
   .then(data => dummyIdeas(data))
   .catch(err => console.log(err));
 
+
+//fetch image of the post it do and place it in the grid as a background
+function fetchBlob(){
+ let img_url = 'asset/post-it'
+
+ fetch(url)
+ .then(response => response.blob())
+ .then(pic => girdPic(pic))
+ .catch(err => console.log(err));
+
+ let postNote = URL.createObjectURL(pic)
+
+ dummyIdeas(postNote, data);
+}
+
 function dummyIdeas(data) {
+  //showing ideas in a grid as layout for the page
   let something = document.getElementById('placeholder');
   for (let i = 0; i < data.length; i++) {
     var div = document.createElement("div");
+    div.className += "grid-item"
     div.innerHTML = 'Hey Check this out: ' + data[i].title + '<br> ' + data[i].body;
     something.appendChild(div);
     
@@ -28,3 +46,5 @@ function dummyIdeas(data) {
   
 }
 
+
+//showing the post it note to show it behind the text
